26eb727af6e2a8f0db2951d771a3e661a9fe0dcd
halen-tech <cory@stoogelabs.com>
2019-02-22 18:58:49 -0800

    god

--- lixie_test/lixie_test.ino
+++ lixie_test/lixie_test.ino
@@ -42,6 +42,9 @@ unsigned long buttonDownTime = 0;
 float brightness = 0.0;
 
 #include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 const char* ssid     = "4wire";
 const char* password = "laserlaser";
@@ -76,6 +79,34 @@ void setup() {
     Serial.println("WiFi connected.");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+
+    ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+    ArduinoOTA.begin();
     
     server.begin();
     
@@ -138,6 +169,9 @@ boolean checkModeChange() {
 }
 
 void loop() {
+
+  ArduinoOTA.handle();
+  
   WiFiClient client = server.available();   // listen for incoming clients
 
   if (client) {                             // if you get a client,
@@ -593,7 +627,3 @@ void colorWipe9(RgbColor c, uint8_t wait) {
 //RgbColor colornew(r, g, b);
 //    return colornew;
 //}
-
-
-
-
